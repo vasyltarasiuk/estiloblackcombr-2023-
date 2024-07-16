b7b121297b3402cad1fd29690623c975a90e8462
vasyltarasiuk <vasyl.tarasiuk@zeeks.co>
2024-07-16 11:33:23 +0300

    Add files via upload

---
+++ js/300/addthis_widget.js
@@ -0,0 +1,29 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+//  AddThis services have been deprecated as of 5/31/23
+
+
+}
+
+/*
+playback timings (ms):
+  captures_list: 0.96
+  exclusion.robots: 0.044
+  exclusion.robots.policy: 0.026
+  esindex: 0.017
+  cdx.remote: 9.697
+  LoadShardBlock: 647.447 (6)
+  PetaboxLoader3.datanode: 388.964 (7)
+  PetaboxLoader3.resolve: 338.974 (3)
+  load_resource: 193.195
+*/
